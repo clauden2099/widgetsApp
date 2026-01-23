65d9eb6dbfd54f75bb26381bd730b9ae79a3ae9a
Alan <alanjavier852@gmail.com>
2026-01-22 23:39:45 -0600

    Se implemento la home screen

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,6 +10,52 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Flutter + Material 3'),
       ),
+      body: _HomeView()
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    /*Cuando un widget tiene se instancia con un builder significa que ese 
+    widget no recibe directamente un hijo (child) ya construido, sino que 
+    recibe una función que construye el widget bajo demanda.
+  */
+    return ListView.builder(
+      itemCount: appMenuItems.length,
+      itemBuilder: (context, index){
+      final menuItem = appMenuItems[index];
+      /*Los tiles son widgets ya prearmados o configurados para que se vean
+      bien en un listView por que ya tienen una estrucutra de la información
+      que llevaran estos */
+      return _CustmListTile(menuItem: menuItem);
+    }
+    );
+  }
+}
+
+class _CustmListTile extends StatelessWidget {
+  const _CustmListTile({
+    required this.menuItem,
+  });
+
+  final MenuItem menuItem;
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return ListTile(
+      leading: Icon(menuItem.icon, color:colors.primary),
+      trailing: Icon(Icons.arrow_forward_ios_rounded, color: colors.primary),
+      title: Text(menuItem.title),
+      subtitle: Text(menuItem.subTitle),
+      onTap: (){
+        
+      },
     );
   }
 }
