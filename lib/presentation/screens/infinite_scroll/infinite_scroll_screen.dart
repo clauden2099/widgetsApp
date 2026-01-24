99cc9734c020f0a0c1e4d1a834e1fc498c0c041e
Alan <alanjavier852@gmail.com>
2026-01-24 00:45:12 -0600

    Se crearon la estrucutra de carpetas para las demas pantallas y se actualizo el barrir de importaciones con los archivos de estas

---
+++ lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class InfiniteScollScreen extends StatelessWidget {
+  static const name = 'infinite_screen';
+  const InfiniteScollScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
